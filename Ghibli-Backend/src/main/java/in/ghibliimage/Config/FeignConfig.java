946f353e995783ee798a43d5bad5590b4c317ec9
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-06-21 07:01:54 +0530

    Created Feign Configuration Class

--- Ghibli-Backend/src/main/java/in/ghibliimage/Config/FeignConfig.java
+++ Ghibli-Backend/src/main/java/in/ghibliimage/Config/FeignConfig.java
@@ -1,4 +1,18 @@
 package in.ghibliimage.Config;
 
+import feign.codec.Encoder;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class FeignConfig {
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringEncoder (
+                () -> new HttpMessageConverters ( new RestTemplate ( ).getMessageConverters ( ) )
+        );
+    }
 }
