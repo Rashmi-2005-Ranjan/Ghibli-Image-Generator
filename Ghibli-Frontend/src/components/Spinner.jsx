a26ec1572cddd122c4c56f4f24737962f7c663bd
Rashmi Ranjan Behera <rbstudu2022@gmail.com>
2025-06-22 12:14:15 +0530

    Delete Ghibli-Frontend directory Due To VCS Error

--- Ghibli-Frontend/src/components/Spinner.jsx
+++
@@ -1,7 +0,0 @@
-const Spinner = () => (
-    <div className="flex justify-center items-center h-full">
-        <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-gray-900"></div>
-    </div>
-);
-
-export default Spinner;
